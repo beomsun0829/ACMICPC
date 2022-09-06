aaf07fe6ec3c887ed84bb33675e915fe6ca3a11f
Hong Beomsun <ghsqjatns@naver.com>
2022-09-06 14:39:41 +0900

    푼거올림

---
+++ 15650 - N과 M (2)/main.py
@@ -0,0 +1,37 @@
+
+
+
+
+
+def dfs(arr):
+    if len(arr) == M:
+        for k in range(len(arr)):
+            print(arr[k], end = ' ')
+        print()
+        return
+    
+    for i in range(1, N + 1):
+        if i in arr:
+            continue
+        
+        if len(arr) == 0:
+            arr.append(i)
+            dfs(arr)
+            arr.pop()
+        
+        elif arr[-1] < i:
+            arr.append(i)
+            dfs(arr)
+            arr.pop()
+    
+    return
+
+def main():
+    global N,M
+    N,M = map(int,input().split())
+    
+    dfs([])
+    
+    
+    
+main()
